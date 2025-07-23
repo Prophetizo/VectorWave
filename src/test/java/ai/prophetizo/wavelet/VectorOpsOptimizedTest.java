e2643a1b065dbf28944125c36b3c83c7a6874a47
Stephen Romano <thestephenromano@gmail.com>
2025-07-23 17:36:25 -0400

    fix test

--- src/test/java/ai/prophetizo/wavelet/VectorOpsOptimizedTest.java
+++ src/test/java/ai/prophetizo/wavelet/VectorOpsOptimizedTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
